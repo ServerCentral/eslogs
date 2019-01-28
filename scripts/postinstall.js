e7f44c5b9d4232e29f62377541d6da23c1afd68a
John Susek <john@johnsolo.net>
2019-01-27 22:17:35 -0600

    Fix issue with postinstall.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,5 +5,5 @@ let path = require('path');
 let homedir = require('os').homedir();
 
 if (!fs.existsSync(`${homedir}/.eslogs.json`)) {
-  fs.createReadStream(path.resolve(__dirname, 'eslogs.json')).pipe(fs.createWriteStream(`${homedir}/.eslogs.json`));
+  fs.createReadStream(path.resolve(__dirname, '../eslogs.json')).pipe(fs.createWriteStream(`${homedir}/.eslogs.json`));
 }
