a547c655b19f8353d2bf79112b04271c21fd5b26
John Susek <john@johnsolo.net>
2019-02-12 14:22:53 -0600

    Get rid of postinstall script

--- scripts/postinstall.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-let fs = require('fs');
-let path = require('path');
-let homedir = require('os').homedir();
-
-if (!fs.existsSync(`${homedir}/.eslogs.json`)) {
-  fs.createReadStream(path.resolve(__dirname, '../eslogs.json')).pipe(fs.createWriteStream(`${homedir}/.eslogs.json`));
-}
